58ed7ce44bce29c91bcc67061568226a74305ac1
Pureewat Kaewpoy <pureewat.k@gmail.com>
2017-11-26 11:03:56 +0700

    re structure

--- Route.js
+++ Route.js
@@ -1,31 +1,63 @@
 var express = require('express')
-//var eieiHandler = require('./controllers/eieiHandler')
-var bodyParser=require('body-parser')
-var pconBuild =require('./ProductQuery')
-var dynamic=require('./Dynamic')
-var app=express()
-/*app.use('/',function(req, res, next){
-    res.json({DS: 'close'});
-    next();
-})*/
-//app.use(bodyParser.json());
-//app.post('/product',pconBuild.proAll)
-//app.get('/',function(req,res){
-    
-    //es.send('LOL')
-//})
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
-//pp.post('/',function(req,res){
-    //res.send('POST request to the homepage')
-//)
+const query = require('./query')
 
+app.get('/product', (req, res) => {
+  if(req.query.name == undefined) {
+    query.selectAll('product')(req, res)
+  } else {
+    query.productName(req, res)
+  }
+})
+app.get('/product/:id', query.selectByID('product', 'productNo'))
+app.post('/product', query.insertInto('product'))
+app.patch('/product/:id', query.updateTable('product', 'productNo'))
+app.delete('/product/:id', query.deleteByID('product', 'productNo'))
 
-app.get('/:table',dynamic.dynamicAll)
-app.get('/:table/:id/:condition',dynamic.dynamicIdCall)
-app.post('/:table',dynamic.dynamicInsert)
-app.patch('/:table/:id',dynamic.dynamicUpdate)
-app.delete('/:table/:id',dynamic.dynamicDelete)
-app.get('/staffMVP/method',dynamic.staffMVP)
-app.get('/product?name',dynamic.productName)
+app.get('/staffs', query.selectAll('staffs'))
+app.get('/staffs/:id', query.selectByID('staffs', 'staffNo'))
+app.post('/staffs', query.insertInto('staffs'))
+app.patch('/staffs/:id', query.updateTable('staffs', 'staffNo'))
+app.delete('/staffs/:id', query.deleteByID('staffs', 'staffNo'))
+
+app.get('/producttype', query.selectAll('producttype'))
+app.get('/producttype/:id', query.selectByID('producttype', 'productTypeNo'))
+app.post('/producttype', query.insertInto('producttype'))
+app.patch('/producttype/:id', query.updateTable('producttype', 'productTypeNo'))
+app.delete('/producttype/:id', query.deleteByID('producttype', 'productTypeNo'))
+
+app.get('/productbill', query.selectAll('productbill'))
+app.get('/productbill/:id', query.selectByID('productbill', 'productBillNo'))
+app.post('/productbill', query.insertInto('productbill'))
+app.patch('/productbill/:id', query.updateTable('productbill', 'productBillNo'))
+app.delete('/productbill/:id', query.deleteByID('productbill', 'productBillNo'))
+
+app.get('/members', query.selectAll('members'))
+app.get('/members/:id', query.selectByID('members', 'memberNo'))
+app.post('/members', query.insertInto('members'))
+app.patch('/members/:id', query.updateTable('members', 'memberNo'))
+app.delete('/members/:id', query.deleteByID('members', 'memberNo'))
+
+app.get('/bills', query.selectAll('bills'))
+app.get('/bills/:id', query.selectByID('bills', 'billNo'))
+app.post('/bills', query.insertInto('bills'))
+app.patch('/bills/:id', query.updateTable('bills', 'billNo'))
+app.delete('/bills/:id', query.deleteByID('bills', 'billNo'))
+
+app.get('/accounts', query.selectAll('accounts'))
+app.get('/accounts/:id', query.selectByID('accounts', 'accountNo'))
+app.post('/accounts', query.insertInto('accounts'))
+app.patch('/accounts/:id', query.updateTable('accounts', 'accountNo'))
+app.delete('/accounts/:id', query.deleteByID('accounts', 'accountNo'))
+
+app.post('/report/totalsale/day', query.priceSum)
+// app.get('/staffMVP/method', dynamic.staffMVP)
+// app.get('/product', dynamic.productName)
 
 app.listen(9000)
+console.log('listen on 9000')
---
+++ query.js
@@ -0,0 +1,137 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: '103.86.50.169',
+    user: 'doge_pos',
+    password: 'JRuMykJ3uMtPeoAh',
+    database: 'doge_pos'
+})
+connection.connect()
+
+const selectAll = (table) => (req, res) => {
+  let sql = `SELECT * FROM ${table}`
+  console.log(sql)
+  connection.query(sql, (err, rows, field) => {
+    if (err) res.send(err)
+    else res.send({
+      data: rows
+    })
+  })
+}
+
+const selectByID = (table, primaryKeyName) => (req, res) => {
+  let sql = `SELECT * FROM ${table} WHERE ${primaryKeyName} = ${req.params.id}`
+  connection.query(sql, (err, rows, field) => {
+    if (err) res.send(err)
+    else res.send({
+      data: rows[0]
+    })
+  })
+}
+
+const insertInto = (table) => (req, res) => {
+  let obj = req.body.data
+  let sql = `Insert into ${table} `
+  let valueString = '('
+  let columnString = '('
+  Object.keys(obj).forEach((key, i) => {
+    if (i == Object.keys(obj).length - 1) {
+      columnString += key
+      valueString += obj[key]
+    }
+    else {
+      columnString += key + ','
+      valueString += obj[key] + ','
+    }
+  })
+  columnString += ')'
+  valueString += ')'
+  sql += `${columnString} VALUES ${valueString}`
+  connection.query(sql, (err, rows) => {
+    if (err) res.send(err)
+    else res.send({
+      data: rows
+    })
+  })
+}
+
+const updateTable = (table, primaryKeyName) => (req, res) => {
+  let obj = req.body.data
+  let sql = `UPDATE ${table} SET `
+  Object.keys(obj).forEach((key, i) => {
+    if (i == Object.keys(obj).length - 1) {
+      sql += `${key} = ${obj[key]} `
+    } else
+      sql += `${key} = ${obj[key]}, `
+  })
+  sql += `Where ${primaryKeyName} = ${req.params.id}`
+  connection.query(sql, (err, rows) => {
+      if (err) res.send(err)
+      else res.send({
+        data: rows
+      })
+  })
+}
+
+const deleteByID = (table, primaryKeyName) => (req, res) => {
+  const id = req.params.id
+  let sql = `DELETE FROM ${table} WHERE ${primaryKeyName} = ${id}`
+  connection.query(sql, (err, rows) => {
+    if (err) res.send(err)
+    else res.send({
+      data: rows
+    })
+  })
+}
+
+const staffMVP = (req, res) => { //พนักงานที่ขายของเยอะสุด
+    let sql = 'SELECT s.* FROM staffs s join bills b on s.staffNo=b.staffs_staffNo join productbills on bills_billNo=productbills.billNoGROUP BY staffNo HAVING SUM(price) =  (SELECT MAX(b.num) FROM (SELECT SUM(price) as "num" FROM bills join staffs on bills.staffs_staffNo =staffs.staffNo join productbills on productbills.bills_billNo = bills.billNo GROUP BY staffNo) b  )'
+    console.log(sql)
+    connection.query(sql, function (err, rows) {
+        if (err) throw (err)
+        res.send(rows)
+    })
+}
+
+const priceSum = (req, res) => { //For หายอดขายในแต่ละวันหรือวันที่ส่งมา
+  let obj = req.body;
+  let start = obj.data.start
+  let end = obj.data.end
+  let sql = "SELECT SUM(price) AS 'total' FROM productbills pb join bills b on pb.BILLS_billNo=b.billNo WHERE b.dateOfBill BETWEEN \"" + start + "\" AND \"" + end + "\""
+  connection.query(sql, (err, rows) => {
+      if (err) res.send(err)
+      res.send({
+        data: rows[0]
+      })
+  })
+}
+
+const productName = (req, res) => {
+    let name = req.query.name
+    let sql = 'Select * FROM product WHERE productName LIKE ' + `"%${name}%"`
+    connection.query(sql, (err, rows) => {
+      if (err) res.send(err)
+      else res.send({
+        data: rows
+      })
+    })
+}
+
+const BestSellid = (req, res) => { //หาที่ขายดีสุดหาตาม id 
+    let table = req.params.table
+    let id = req.params.id
+    let sql = 'Select productName from products p join productBills pb on p.productNo = pb.productBills_productNo join bills b on b.billNo=pb.bills_billNo full join staffs s on s.staffNo = b.staffs_staffNo full join members m on m.memberNo = b.members_memberNo join producttypes pt on pt.productTypeNo = p.productTypes_productTypeNo WHERE ' + table + '=' + id + 'And b.dateOfBill = ' + req.params.date
+    connection.query(sql, function (err, rows) {
+        if (err) throw (err)
+        res.send(rows)
+    })
+}
+
+module.exports = {
+  selectAll,
+  selectByID,
+  insertInto,
+  updateTable,
+  deleteByID,
+  priceSum,
+  productName
+}
